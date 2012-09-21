aa260ba4b010ae9106b020ff9cbe5dfcd3f89ceb
Dan Durkin <ddurkin@broadinstitute.org>
2012-09-21 10:33:10 -0400

    pointed to oracle schema

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,15 +1,19 @@
 dataSource {
+    dbCreate='create-drop'
+    url = 'jdbc:oracle:thin:@vmbarddev:1521:barddev'
+    driverClassName = 'oracle.jdbc.driver.OracleDriver'
+    username = "ddurkin"
+    password = "hPE8eXjCXUi8"
     pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
 }
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
     cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory'
 }
+
 // environment specific settings
+/*
 environments {
     development {
         dataSource {
@@ -41,3 +45,4 @@ environments {
         }
     }
 }
+*/
